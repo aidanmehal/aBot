b43ed499e8173818b13aadd95511e2e008f5ca61
aidanmehal <141237899+aidanmehal@users.noreply.github.com>
2025-02-21 16:41:42 +0000

    ### COMMIT ### - This commit will initialize the first three files i have created for aBot. These files inlcude the main.py, which will run the bot, bot_setup.py, which will initialize the bot, and finally moderation.py which will hold the bots moderation commands.

---
+++ bot_setup.py
@@ -0,0 +1,11 @@
+import discord
+from discord.ext import commands
+
+# Set up bot intents
+intents = discord.Intents.default()
+intents.messages = True
+intents.guilds = True
+intents.members = True
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
